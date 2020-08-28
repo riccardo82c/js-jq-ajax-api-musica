db17926e01f031e5ddba9886db89c7dfde746e47
Riccardo Colombo <rikyregna@gmail.com>
2020-08-28 18:22:55 +0200

    some little adjustment

--- main.js
+++ main.js
@@ -2,29 +2,67 @@
 Servendoci di handlebars stampiamo tutto a schermo.
 In questo momento non è importante la parte grafica. */
 
-
-
 $(document).ready(function () {
 
 	$.ajax({
 		"url": "https://flynn.boolean.careers/exercises/api/array/music",
 		"method": "GET",
 		success: function (data) {
+
+			transformAttr(data.response, 'genre');
+
 			// crea il DOM
 			createDOM(data.response);
 			// crea le opzioni del select DINAMICAMENTE
 			createOptions(data.response);
 
 			// al cambio di input in <select>
-			$('#genre').change(function () {
-				displaySelection();
-			})
+			$('#genre').change(displaySelection());
 		},
 		'error': function (richiesta, stato, errori) {
 			alert("E' avvenuto un errore.");
 		}
 	});
 });
+
+
+/* FUNZIONI */
+
+// Trasforma in lowerCase un attributo di un oggetto in un array
+
+function transformAttr(arr, attr) {
+	for (let i = 0; i < arr.length; i++) {
+		arr[i][attr] = arr[i][attr].toLowerCase();
+	}
+}
+
+// popolazione del DOM
+function createDOM(data) {
+	var source = $('#template').html();
+	var template = Handlebars.compile(source);
+	for (var i = 0; i < data.length; i++) {
+		var html = template(data[i]);
+		$('.cds-container').append(html);
+	}
+}
+
+// Creazione dinamica delle option del select
+function createOptions(data) {
+	$('header').after(`<select id="genre"></select>`);
+	var allCategory = reduceCategory(data, 'genre');
+	allCategory.unshift('all');
+	console.log(allCategory);
+	for (var i = 0; i < allCategory.length; i++) {
+		$('#genre').append(`<option value="${allCategory[i]}">${capitalize(allCategory[i])}</option>`)
+	}
+}
+
+// Visualizza gli items del DOM in funzione del cambio dell'input in select
+function displaySelection() {
+	var category = $('#genre').val();
+	$('.cd').hide();
+	category != 'all' ? $('.cd.' + category).show() : $('.cd').show();
+}
 
 
 // riduce array di oggetti per attributo
@@ -43,33 +81,3 @@ function capitalize(stringa) {
 	var primoCarattere = stringa.charAt(0).toUpperCase();
 	return primoCarattere + stringa.slice(1).toLowerCase();
 };
-
-
-// popolazione del DOM
-function createDOM(data) {
-	var source = $('#template').html();
-	var template = Handlebars.compile(source);
-	for (var i = 0; i < data.length; i++) {
-		var html = template(data[i]);
-		$('.cds-container').append(html);
-	}
-}
-
-
-// Creazione dinamica delle option del select
-function createOptions(data) {
-	$('header').after(`<select id="genre"></select>`);
-	var allCategory = reduceCategory(data, 'genre');
-	allCategory.unshift('All');
-	console.log(allCategory);
-	for (var i = 0; i < allCategory.length; i++) {
-		$('#genre').append(`<option value="${allCategory[i]}">${capitalize(allCategory[i])}</option>`)
-	}
-}
-
-
-function displaySelection() {
-	var category = $('#genre').val();
-	$('.cd').hide();
-	category != 'All' ? $('.cd.' + category).show() : $('.cd').show();
-}
